8746a63cf8365edffd838497bc2e004ee7c22786
khongchai <world1955@hotmail.com>
2021-08-09 22:35:32 +0700

    products query and pagination finished

--- ecommerce/schema.py
+++ ecommerce/schema.py
@@ -2,12 +2,13 @@
 #DjangoObjectType automatically generate types. We'll just have to expose them as fields now
 #this is like urls.py
 import graphene
+from django.contrib.postgres.search import  SearchVector
+from graphene.types.scalars import Boolean
 from graphene_django import DjangoObjectType
-from store.models import Product, DataAfterPurchase, Composer, Composition
-from graphql_auth.schema import UserQuery, MeQuery
 from graphql_auth import mutations
-from django.contrib.postgres.search import SearchQuery, SearchVector
-
+from graphql_auth.schema import MeQuery, UserQuery
+from django.core.paginator import Paginator
+from store.models import Composer, Composition, DataAfterPurchase, Product
 
 class ProductType(DjangoObjectType):
     class Meta: 
@@ -29,23 +30,22 @@ class DataAfterPurchaseType(DjangoObjectType):
         model = DataAfterPurchase
         fields = "__all__"
 
-"""
-graphene.ObjectType gets exposed to graphql.
-"""
+class PagePositionType(graphene.ObjectType):
+    page = graphene.Int()
+    of = graphene.Int()
+
+class AllProductsDataType(graphene.ObjectType):
+    products = graphene.List(ProductType)
+    is_first = graphene.Boolean()
+    is_last = graphene.Boolean()
+    page_position = graphene.Field(PagePositionType)
+
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
     token_auth = mutations.ObtainJSONWebToken.Field()
     update_account = mutations.UpdateAccount.Field()
     refresh_token = mutations.RefreshToken.Field()
-
-class ProductPaginatedType(graphene.ObjectType):
-    page = graphene.Int()
-    pages = graphene.Int()
-    skip = graphene.Int()
-    is_first = graphene.Boolean()
-    is_last = graphene.Boolean()
-    objects = graphene.List(ProductType)
 
 class ComposerQuery(graphene.ObjectType):
     all_composers_info = graphene.List(ComposerType)
@@ -61,31 +61,45 @@ class CompositionQuery(graphene.ObjectType):
         all_compositions = Composition.objects.prefetch_related("composers").all() 
         return all_compositions
 
-class Query(UserQuery, MeQuery, ComposerQuery, CompositionQuery, graphene.ObjectType):
-    all_products_info = graphene.List(ProductType, search=graphene.String(required=False))
-    all_products_info_paginated = graphene.Field(ProductPaginatedType)
-    all_data_after_purchase_only = graphene.List(DataAfterPurchaseType)
+class ProductsQuery(graphene.ObjectType):
+    all_products_info = graphene.Field(AllProductsDataType, search=graphene.String(required=False), 
+                        limit=graphene.Int(required=False), page=graphene.Int(required=False))
 
-    product_by_name = graphene.Field(ProductType, name=graphene.String(required=True))
+    def resolve_all_products_info(root, _, search, limit, page):
+        limit = limit if limit else 9
+        page = max(page, 1) if page else 1
 
-    def resolve_all_products_info(root, info, search):
-        all_products = Product.objects.select_related("composition").all()
+        all_products = Product.objects.select_related("composition").all().order_by("composition__name")
         filtered_products = all_products.annotate(
                             search=SearchVector("composition__name", "composition__composers__name", config="unaccent")
-                            ).filter(search__icontains=search)
-        return filtered_products
+                            ).filter(search__icontains=search) if search else all_products
+        paginator = Paginator(filtered_products, limit)
+        paginated_products = paginator.page(page)
+        return_info = {
+            "products": paginated_products.object_list,
+            # Do not check against page number as page nums 
+            # are dynamic and can change (eg. starting at page 0).
+            "is_first": not paginated_products.has_previous(),
+            "is_last": not paginated_products.has_next(),
+            "page_position": {
+                "page": page,
+                "of": paginator.count
+            }
+        }
+        return return_info
 
-    def resolve_product_by_name(root, info, name):
+class Query(UserQuery, MeQuery, ComposerQuery, ProductsQuery, CompositionQuery, graphene.ObjectType):
+   
+    all_data_after_purchase_only = graphene.List(DataAfterPurchaseType)
+    product_by_name = graphene.Field(ProductType, name=graphene.String(required=True))
+    def resolve_product_by_name(root, _, name):
         try:
             return Product.objects.get(name=name)
         except Product.DoesNotExist:
             return None
     
-    def resolve_all_products_info_paginated(root, info):
-        return "TODO"
-
     #hide this in prod
-    def all_data_after_purchase_only(root, info):
+    def all_data_after_purchase_only(root, _):
         return DataAfterPurchase.objects.all()
 
 class Mutation(AuthMutation, graphene.ObjectType):
