136f9469e9b9d32f9a6937827118e96cdfda395d
khongchai <world1955@hotmail.com>
2021-08-30 21:25:07 +0700

    removed meQuery

--- ecommerce/schema.py
+++ ecommerce/schema.py
@@ -1,17 +1,18 @@
 import graphene
-from ecommerce.graphene_queries.me_queries_extended import \
-    MeQueryExtended
-from graphql_auth.schema import MeQuery, UserQuery
+from graphql_auth.schema import UserQuery
+
+from ecommerce.graphene_queries.me_queries_extended import MeQueryExtended
 
 from .graphene_mutations.cart_mutations import CartMutations
-from .graphene_mutations.user_mutations import AuthMutations, CustomAuthMutations
+from .graphene_mutations.user_mutations import (AuthMutations,
+                                                CustomAuthMutations)
 from .graphene_queries.cart_queries import CartsQuery
 from .graphene_queries.store_queries import (ComposersQuery, CompositionsQuery,
                                              DataAfterPurchaseQuery,
                                              ProductsQuery)
 
 
-class Query(UserQuery, MeQuery, MeQueryExtended, ComposersQuery, ProductsQuery, 
+class Query(UserQuery, MeQueryExtended, ComposersQuery, ProductsQuery, 
             CompositionsQuery, DataAfterPurchaseQuery, 
             CartsQuery, graphene.ObjectType):
     pass
--- notes.md
+++ notes.md
@@ -1,11 +1,3 @@
-environment variable for github pages
-
-look at how to handle that with nextjs => https://youtu.be/I6ypD7qv3Z8?t=16756
-
-take a look at TODO in user_mutations.py
-
-forgot password functionality!
-
-Don't forget the midilLink!
+Field for a link to metronome file.
 
 make actual fixtures && deploy
