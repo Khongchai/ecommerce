9b16e76c12a25663f9b184a84577251c42f325c6
khongchai <world1955@hotmail.com>
2021-09-07 21:59:56 +0700

    changed database settings for production

--- ecommerce/settings_production.py
+++ ecommerce/settings_production.py
@@ -9,7 +9,9 @@ CORS_ALLOWED_ORIGINS = []
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-DATABASES["default"] = dj_database_url.config()
+DATABASES = {
+    "default": dj_database_url.config()
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
